e19822f777f2ce4c8c9229bd2ba8282b98a58fdd
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2017-11-07 05:16:58 -0500

    Update Query5.sql

--- Project4/MySQL Queries/Query5.sql
+++ Project4/MySQL Queries/Query5.sql
@@ -1,4 +1,4 @@
 /*List the names of students who have taken a course from department v6 (deptId), but not v7.*/
 select s.name
 from student as s USE INDEX (), transcript as t USE INDEX (), course as c USE INDEX ()
-where s.id = t.studId and (t.crsCode= c.crsCode and deptId="deptId626720" and deptId!="deptId684634"); 
+where s.id = t.studId and (t.crsCode= c.crsCode and deptId="deptId324641" and deptId!="deptId684634"); 
