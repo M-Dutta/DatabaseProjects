04dab5141e5a13cbfa3fd81168fa38cfba81e846
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2017-11-07 05:15:30 -0500

    Update Query2.sql

--- Project4/MySQL Queries/Query2.sql
+++ Project4/MySQL Queries/Query2.sql
@@ -1,6 +1,6 @@
 /*List the names of students with id in the range of v2 (id) to v3 (inclusive).*/
 SELECT s.name
 FROM student as s USE INDEX ()
-where S.id >= 1000 And S.id <=9000;
+where s.id >= 10000 And s.id <=90000;
 
 
