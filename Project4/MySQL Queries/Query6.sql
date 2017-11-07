f847ac2cda45e1806b122925fdc48043173edd8c
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2017-11-07 05:17:18 -0500

    Update Query6.sql

--- Project4/MySQL Queries/Query6.sql
+++ Project4/MySQL Queries/Query6.sql
@@ -1,4 +1,4 @@
 /*List the names of students who have taken all courses offered by department v8 (deptId)*/
 select s.name
 from student as s USE INDEX (), transcript as t USE INDEX (), course as c USE INDEX ()
-where c.deptId="deptId626720" and c.crsCode = t.crsCode and s.id = t.studId;
+where c.deptId="deptId324641" and c.crsCode = t.crsCode and s.id = t.studId;
