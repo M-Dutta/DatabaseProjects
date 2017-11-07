8339b1f65fd36efbb0f5855da8aad4a6ef84d0c2
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2017-11-07 05:14:51 -0500

    Update Query1.sql

--- Project4/MySQL Queries/Query1.sql
+++ Project4/MySQL Queries/Query1.sql
@@ -1,6 +1,4 @@
 /* List the name of the student with id equal to v1 (id).*/
 SELECT s.name
-FROM student as S USE INDEX()
-where s.id =501434;
-
-
+FROM student as S USE INDEX ()
+where s.id =737270;
