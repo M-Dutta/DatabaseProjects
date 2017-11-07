8951007c9ca9dcbfb9a3226cdaf6f2e374cf24a2
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2017-11-07 04:20:15 -0500

    Update Query4.sql

--- Project4/MySQL Queries/Query4.sql
+++ Project4/MySQL Queries/Query4.sql
@@ -1,4 +1,4 @@
 /*List the names of students who have taken a course taught by professor v5 (name)*/
 select s.name
-from student as s, professor as p, teaching as t, transcript as tr
-where p.name = "name283155" and  s.id = tr.studId and tr.crsCode = t.crsCode and t.semester = tr.semester 
+from student as s USE INDEX (), professor as p USE INDEX (), teaching as t USE INDEX (), transcript as tr USE INDEX ()
+where p.name = "name283155" and  s.id = tr.studId and tr.crsCode = t.crsCode and t.semester = tr.semester;
