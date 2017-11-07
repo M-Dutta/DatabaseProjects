1ec79425895ef22da3b4495604165787a8db81bf
M-Dutta <33167132+M-Dutta@users.noreply.github.com>
2017-11-07 05:16:09 -0500

    Update Query3.sql

--- Project4/MySQL Queries/Query3.sql
+++ Project4/MySQL Queries/Query3.sql
@@ -1,4 +1,4 @@
 /*List the names of students who have taken course v4 (crsCode).*/
 Select s.name
 from student as s USE INDEX (), transcript as t USE INDEX ()
-where t.crsCode="crsCode106349" and s.id = t.studId;
+where t.crsCode="crsCode685200" and s.id = t.studId;
